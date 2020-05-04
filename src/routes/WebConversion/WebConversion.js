e4d25d18e1aec6c501dfb4e1298b06b1fd0574fe
Sophia Li <sophiali124@gmail.com>
2020-05-04 13:57:03 -0700

    update web conversion

--- src/routes/WebConversion/WebConversion.js
+++ src/routes/WebConversion/WebConversion.js
@@ -1,20 +1,22 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import cheerio from "cheerio";
 
-class App extends Component {
-  state = { names: [] };
+const WebConversion = () => {
+  const [names, setNames] = useState([]);
 
-  componentDidMount() {
+  useEffect(() => {
     axios
       .get(
         "https://cors-anywhere.herokuapp.com/https://www.allrecipes.com/recipe/20144/banana-banana-bread/"
       )
       .then((html) => {
-        let names = [];
         let $ = cheerio.load(html.data);
         console.log(html.data);
         console.log(cheerio.load(html));
+
+        // console.log($("title").text());
+
         $(".ingredients-section li span .ingredients-item-name").each(function (
           i,
           element
@@ -23,26 +25,23 @@ class App extends Component {
           names.push(name);
         });
 
-        this.setState({ names });
+        setNames([names]);
       })
       .catch(function (err) {
         console.log("crawl failed");
       });
-  }
+  }, []);
 
-  render() {
-    return (
-      <div>
-        <input type="text" />
-        <button>Submit</button>
-        <ul>
-          {this.state.names.map((name) => {
-            return <li key={name}>{name}</li>;
-          })}
-        </ul>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <ul>
+        {names.map((name) => {
+          return <li key={name}>{name}</li>;
+        })}
+      </ul>
+      {/* <ul>{names}</ul> */}
+    </div>
+  );
+};
 
-export default App;
+export default WebConversion;
