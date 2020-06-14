09c1882940020928e490c48d0553cbee52dd1ccd
Sophia Li <sophiali124@gmail.com>
2020-06-14 13:39:02 -0700

    update temp formatting

--- src/routes/Temperature/CelsiusToF.js
+++ src/routes/Temperature/CelsiusToF.js
@@ -2,11 +2,16 @@ import React, { useState } from 'react'
 
 import styles from './Temperature.module.css'
 
-const CelsiusToF = ({ updateCelsius }) => {
+const CelsiusToF = () => {
   const [celsiusToF, setCelsiusToF] = useState('')
+  const [celsius, setCelsius] = useState('')
 
   function handleChangeCelsiusToF(event) {
     setCelsiusToF(event.target.value)
+  }
+
+  function updateCelsius(tempC) {
+    setCelsius(tempC)
   }
 
   function handleSubmitCelsiusToF(event) {
@@ -14,23 +19,27 @@ const CelsiusToF = ({ updateCelsius }) => {
     updateCelsius(`${Math.round(celsiusToF * (9 / 5) + 32)} °F`)
     setCelsiusToF('')
   }
+
   return (
-    <div className={styles.temperatureContainer}>
-      <form onSubmit={handleSubmitCelsiusToF}>
-        <label>
-          <h3>Celcius to Farenheit: </h3>
-        </label>
-        <input
-          type="number"
-          value={celsiusToF}
-          onChange={handleChangeCelsiusToF}
-          className={styles.input}
-          placeholder="e.g. 175"
-        />
-        <button type="submit" className={styles.submitButton}>
-          Convert to Farenheit
-        </button>
-      </form>
+    <div className={styles.backgroundcolorContainer}>
+      <div className={styles.temperatureContainer}>
+        <form onSubmit={handleSubmitCelsiusToF}>
+          <label>
+            <h3>Celcius to Farenheit: </h3>
+          </label>
+          <input
+            type="number"
+            value={celsiusToF}
+            onChange={handleChangeCelsiusToF}
+            className={styles.input}
+            placeholder="e.g. 175"
+          />
+          <button type="submit" className={styles.submitButton}>
+            Convert to Farenheit
+          </button>
+        </form>
+        <p>Result: {celsius}</p>
+      </div>
     </div>
   )
 }
--- src/routes/Temperature/Temperature.js
+++ src/routes/Temperature/Temperature.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import FarenheitToC from './FarenheitToC'
 import CelsiusToF from './CelsiusToF'
 import Layout from '../../components/Layout'
@@ -6,26 +6,14 @@ import Layout from '../../components/Layout'
 import styles from './Temperature.module.css'
 
 const Temperature = ({ updateFtoC }) => {
-  const [farenheit, setFarenheit] = useState('')
-  const [celsius, setCelsius] = useState('')
-
-  function udpdateFarenheit(tempF) {
-    setFarenheit(tempF)
-  }
-
-  function updateCelsius(tempC) {
-    setCelsius(tempC)
-  }
-
   return (
     <Layout>
-      <div className={styles.container}>
-        <h2>Temperature Converter</h2>
+      <div>
+        <h2 className={styles.title}>Temperature Converter</h2>
 
-        <FarenheitToC udpdateFarenheit={udpdateFarenheit} />
-        <p>Result: {farenheit}</p>
-        <CelsiusToF updateCelsius={updateCelsius} />
-        <p>Result: {celsius}</p>
+        <FarenheitToC />
+
+        <CelsiusToF />
       </div>
     </Layout>
   )
